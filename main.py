b143ac7ecab2587a7d4a06c76ec0bae6cbb223e4
AndreaCiv <s1092302@studenti.univpm.it>
2023-02-02 11:14:37 +0100

    create main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from pyswip import Prolog
+
+if __name__ == "__main__":
+    prolog = Prolog()
+    prolog.consult("knowledge_base_file_definitivo.pl")
+    risposta = list(prolog.query("create_semester_schedule(\"Ingegneria Informatica\", 1,1,X)."))
+    print(risposta)

490acef3c93e5ebe3ccd49ab4ca6d197b6868402
Gioele <gioele.pasquini101@asu.ulpgc.es>
2023-02-03 11:29:10 +0100

    Aggiunte dipendenze

--- ScheduleView.py
+++ ScheduleView.py
@@ -1,8 +1,13 @@
-from PyQt5.QtWidgets import QDialog
-
+from Ui_Schedule import Ui_Schedule
+from PyQt6.QtWidgets import QDialog
 class ScheduleView(QDialog):
     def __init__(self):
         super(QDialog, self).__init__()
 
         self.ScheduleView = Ui_Schedule()
         self.ScheduleView.setupUi(self)
+
+    def show_schedule(self):
+        self.ScheduleView.setupUi(self)
+        self.show()
+
